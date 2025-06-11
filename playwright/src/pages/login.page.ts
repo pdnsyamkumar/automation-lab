53b684f8030e656bb3e250dbf826bd9609f3dc39
Syam Kumar PDN <pdnsktesting123@gmail.com>
2025-06-11 22:56:53 +0530

    `Added loginPage fixture to page.fixtures.ts`

---
+++ playwright/src/pages/login.page.ts
@@ -0,0 +1,13 @@
+import { BasePage } from "@pages/base.page";
+
+export class LoginPage extends BasePage {
+    readonly getLoginLabel = () => this.page.getByRole('heading', { name: 'Login to your account' });
+    readonly getEmailAddressInput = () => this.page.getByTestId('login-email');
+    readonly getPasswordInput = () => this.page.getByTestId('login-password');
+    readonly getLoginButton = () => this.page.getByTestId('login-button');
+
+    async fillLoginForm(email: string, password: string) {  
+        await this.getEmailAddressInput().fill(email);
+        await this.getPasswordInput().fill(password);
+    }
+}
--- playwright/src/pages/page.fixtures.ts
+++ playwright/src/pages/page.fixtures.ts
@@ -1,9 +1,14 @@
 import { test as baseTest } from '@playwright/test'
+import { LoginPage } from '@pages/login.page'
 import { SignUpPage } from '@pages/sign-up.page'
 import { HomePage } from '@pages/home.page'
 import { TopNavigationPage } from '@pages/top-navigation.page'
 
 export const pageFixtures = baseTest.extend<PageTypes>({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page))
+  },
+
   signUpPage: async ({ page }, use) => {
     await use(new SignUpPage(page))
   },
@@ -11,12 +16,14 @@ export const pageFixtures = baseTest.extend<PageTypes>({
   homePage: async ({ page }, use) => {
     await use(new HomePage(page))
   },
+
   topNavigationPage: async ({ page }, use) => {
     await use(new TopNavigationPage(page))
   },
 })
 
 export interface PageTypes {
+  loginPage: LoginPage
   signUpPage: SignUpPage
   homePage: HomePage
   topNavigationPage: TopNavigationPage
