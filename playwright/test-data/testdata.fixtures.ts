2b46cbe52d640016980a9aa63dd3470fcc5d7ce5
pdnsyamkumar <pdnsktesting123@gmail.com>
2025-06-26 21:50:48 +0530

    `Added signupTestData fixture and updated testDataFixtures interface`

--- playwright/test-data/testdata.fixtures.ts
+++ playwright/test-data/testdata.fixtures.ts
@@ -1,12 +1,18 @@
 import { test as baseTest } from '@playwright/test'
-import { ContactUsTestData } from './contact-us.testdata'
+import { ContactUsTestData } from '@testData/contact-us.testdata'
+import { SignUpTestData } from '@testData/signup.testdata'
 
 export const testDataFixtures = baseTest.extend<TestDataTypes>({
+  signupTestData: async ({}, use) => {
+    await use(new SignUpTestData())
+  },
+
   contactUsTestData: async ({}, use) => {
     await use(new ContactUsTestData())
   },
 })
 
 export interface TestDataTypes {
+  signupTestData: SignUpTestData
   contactUsTestData: ContactUsTestData
 }
